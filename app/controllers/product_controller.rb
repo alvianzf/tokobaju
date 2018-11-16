0f11fd27d8c6b632bb6117487eac8aed2d5cbd8c
farkroft <fajarar77@gmail.com>
2018-11-16 11:31:35 +0700

    fix code-styling with rubocop

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,49 +1,52 @@
+# frozen_string_literal: true
+
+# class for controlling model product
 class ProductController < ApplicationController
-    def index
-        product = Product.all
-        if product.present?
-            render json: product, status: :ok
-        else
-            render json: {
-                data: "product 404"
-            }, status: :unprocesable_entity
-        end
+  def index
+    product = Product.all
+    if product.present?
+      render json: product, status: :ok
+    else
+      render json: {
+        data: 'product 404'
+      }, status: :unprocesable_entity
     end
+  end
 
-    def show
-        product = Product.find_by(id: params[:id])
-        if product.present
-            render json: product, status: :ok
-        else
-            render json: {
-                data: "product 404"
-            }, status: :unprocesable_entity
-        end
+  def show
+    product = Product.find_by(id: params[:id])
+    if product.present
+      render json: product, status: :ok
+    else
+      render json: {
+        data: 'product 404'
+      }, status: :unprocesable_entity
     end
+  end
 
-    def create
-        product = Product.new(params.require(:product).permit(:name, :description))
-        if product.save
-            render json: {
-                message: "new product created"
-            }, status: :created
-        else
-            render json:{
-                message: "product not created"
-            }, status: :unprocesable_entity
-        end
+  def create
+    product = Product.new(params.require(:product).permit(:name, :description))
+    if product.save
+      render json: {
+        message: 'new product created'
+      }, status: :created
+    else
+      render json: {
+        message: 'product not created'
+      }, status: :unprocesable_entity
     end
+  end
 
-    def destroy
-        product = Product.find_by(id: params[:id])
-        if product.destroy!
-            render json:{
-                message: "product has been destroyed"
-            }, status: :ok
-        else
-            render json:{
-                message: "failed to destroy product"
-            }, status: :unprocesable_entity
-        end
+  def destroy
+    product = Product.find_by(id: params[:id])
+    if product.destroy!
+      render json: {
+        message: 'product has been destroyed'
+      }, status: :ok
+    else
+      render json: {
+        message: 'failed to destroy product'
+      }, status: :unprocesable_entity
     end
+  end
 end
