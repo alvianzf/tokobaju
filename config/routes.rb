c3c1c3dfd50d33b240e6cd50ebd593b3799234b2
husnul23 <batalkawin.hf@gmail.com>
2018-11-16 15:28:30 +0700

    update postman

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/category", to: "category#index"
+  get "/category/detail", to: "category#show"
+  post "/category", to: "category#create"
+  delete "/category/delete", to: "category#destroy"
+
 end
